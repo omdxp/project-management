09a98f41751ce833d63a86791ec9b996190f8c09
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-07 12:31:55 +0100

    oop rendering

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.querySelector("#project-input")!;
+    this.hostElement = document.querySelector("#app")!;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
